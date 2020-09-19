eac0cc507d16e9ae53b20ca954fac204657ed07d
kourin <kourin1996@gmail.com>
2020-09-19 03:57:20 +0000

    feat: create deposit API client

--- projects/cosmoscan-extension/src/x/cdp/module.ts
+++ projects/cosmoscan-extension/src/x/cdp/module.ts
@@ -1,5 +1,5 @@
 import { CosmosSDK, codec } from 'cosmos-client';
-import { CdpApi, CreateCdpReq } from './api';
+import { CdpApi, CreateCdpReq, DepositCdpReq } from './api';
 
 export function cdpAccountsGet(sdk: CosmosSDK) {
   return new CdpApi(undefined, sdk.url)
@@ -18,4 +18,18 @@ export function cdpPost(sdk: CosmosSDK, req: CreateCdpReq) {
     res.data = codec.fromJSONString(JSON.stringify(res.data));
     return res;
   });
+}
+
+export function cdpOwnerDenomDepositsPost(
+  sdk: CosmosSDK,
+  ownerAddr: string,
+  denom: string,
+  req: DepositCdpReq,
+) {
+  return new CdpApi(undefined, sdk.url)
+    .cdpOwnerDenomDepositsPost(ownerAddr, denom, req)
+    .then((res) => {
+      res.data = codec.fromJSONString(JSON.stringify(res.data));
+      return res;
+    });
 }
--- projects/cosmoscan-extension/src/x/cdp/types/index.ts
+++ projects/cosmoscan-extension/src/x/cdp/types/index.ts
@@ -1,1 +1,2 @@
 export * from './msg-create-cdp';
+export * from './msg-deposit';

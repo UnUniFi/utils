2a22ea31fa7f70118c4bf1b7e8514dc8a284faf2
kourin <kourin1996@gmail.com>
2020-09-23 14:03:45 +0000

    feat: create CDP info page

--- projects/cosmoscan-extension/src/app/cdps/cdp/cdp.component.ts
+++ projects/cosmoscan-extension/src/app/cdps/cdp/cdp.component.ts
@@ -1,7 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { CosmosSDKService } from '@model-ce/index';
+import { AccAddress } from 'cosmos-client';
+import {
+  CDP,
+  CdpParameters,
+  Deposit,
+} from 'projects/cosmoscan-extension/src/x/cdp/api';
+import {
+  cdpCdpsCdpDepositsOwnerDenomGet,
+  cdpCdpsCdpOwnerDenomGet,
+  cdpParametersGet,
+} from 'projects/cosmoscan-extension/src/x/cdp/module';
+import { from, Observable, pipe, zip } from 'rxjs';
+import { map, mergeMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-cdp',
@@ -9,10 +21,44 @@ import { map } from 'rxjs/operators';
   styleUrls: ['./cdp.component.css'],
 })
 export class CdpComponent implements OnInit {
-  keyID$: Observable<string>;
-  constructor(private readonly route: ActivatedRoute) {
-    this.keyID$ = this.route.queryParams.pipe(
-      map((params) => params['key_id']),
+  owner$: Observable<string>;
+  denom$: Observable<string>;
+  params$: Observable<CdpParameters>;
+  cdp$: Observable<CDP>;
+  deposits$: Observable<Deposit[]>;
+
+  constructor(
+    private readonly route: ActivatedRoute,
+    private readonly cosmosSdk: CosmosSDKService,
+  ) {
+    this.owner$ = this.route.params.pipe(map((params) => params['owner']));
+    this.denom$ = this.route.params.pipe(map((params) => params['denom']));
+    const ownerAndDenom$ = zip(this.owner$, this.denom$);
+
+    this.params$ = from(cdpParametersGet(this.cosmosSdk.sdk)).pipe(
+      map((data) => data.result),
+    );
+
+    this.cdp$ = ownerAndDenom$.pipe(
+      mergeMap(([ownerAddr, denom]: string[]) =>
+        cdpCdpsCdpOwnerDenomGet(
+          this.cosmosSdk.sdk,
+          AccAddress.fromBech32(ownerAddr),
+          denom,
+        ),
+      ),
+      map((data) => data.result),
+    );
+
+    this.deposits$ = ownerAndDenom$.pipe(
+      mergeMap(([ownerAddr, denom]: string[]) =>
+        cdpCdpsCdpDepositsOwnerDenomGet(
+          this.cosmosSdk.sdk,
+          AccAddress.fromBech32(ownerAddr),
+          denom,
+        ),
+      ),
+      map((data) => data.result),
     );
   }
 
--- projects/cosmoscan-extension/src/view/cdps/cdp/cdp.component.ts
+++ projects/cosmoscan-extension/src/view/cdps/cdp/cdp.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Deposit } from 'cosmos-client/api';
+import { CDP, CdpParameters } from 'projects/cosmoscan-extension/src/x/cdp/api';
 
 @Component({
   selector: 'view-cdp',
@@ -6,7 +8,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cdp.component.css'],
 })
 export class CdpComponent implements OnInit {
+  @Input()
+  owner?: string;
+
+  @Input()
+  denom?: string;
+
+  @Input()
+  params?: CdpParameters;
+
+  @Input()
+  cdp?: CDP;
+
+  @Input()
+  deposits?: Deposit[];
+
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    setTimeout(() => {
+      console.log('params', this.params);
+      console.log('cdp', this.cdp);
+      console.log('deposits', this.deposits);
+    }, 10000);
+  }
 }

9e610013ff3de3085b58727b57a2aa5476013ef5
Kimura Yu <33382781+KimuraYu45z@users.noreply.github.com>
2020-12-05 09:35:48 +0900

    update

--- projects/website/src/view/app.component.ts
+++ projects/website/src/view/app.component.ts
@@ -3,7 +3,7 @@ import { Observable, combineLatest } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { Router, NavigationEnd } from '@angular/router';
 import { MediaObserver } from '@angular/flex-layout';
-import { MatSidenav } from '@angular/material/sidenav';
+import { MatDrawerMode, MatSidenav } from '@angular/material/sidenav';
 
 @Component({
   selector: 'view-app',
@@ -14,22 +14,30 @@ export class AppComponent implements OnInit {
   @ViewChild('sidenav')
   sidenav!: MatSidenav;
 
-  drawerMode$: Observable<string>;
+  drawerMode$: Observable<MatDrawerMode>;
   drawerOpened$: Observable<boolean>;
 
   constructor(private router: Router, private mediaObserver: MediaObserver) {
-    this.drawerMode$ = this.mediaObserver.media$.pipe(
-      map((change) => (change.mqAlias === 'xs' ? 'over' : 'side')),
-    );
+    this.drawerMode$ = this.mediaObserver
+      .asObservable()
+      .pipe(
+        map((changes) =>
+          changes.find((change) => change.mqAlias === 'xs') ? 'over' : 'side',
+        ),
+      );
 
-    this.drawerOpened$ = this.mediaObserver.media$.pipe(
-      map((change) => (change.mqAlias === 'xs' ? false : true)),
-    );
+    this.drawerOpened$ = this.mediaObserver
+      .asObservable()
+      .pipe(
+        map((changes) =>
+          changes.find((change) => change.mqAlias === 'xs') ? false : true,
+        ),
+      );
 
-    combineLatest(this.drawerMode$, this.router.events).subscribe(
+    combineLatest([this.drawerMode$, this.router.events]).subscribe(
       ([drawerMode, event]) => {
         if (drawerMode === 'over' && event instanceof NavigationEnd) {
-          this.sidenav.close();
+          this.sidenav?.close();
         }
       },
     );

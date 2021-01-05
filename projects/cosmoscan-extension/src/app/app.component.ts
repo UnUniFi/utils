8ec2c8bd89ca162af19155ece6793df5c527db7e
Kimura Yu <33382781+KimuraYu45z@users.noreply.github.com>
2021-01-05 20:18:59 +0900

    update

--- projects/cosmoscan-extension/src/app/app.component.ts
+++ projects/cosmoscan-extension/src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import * as config from '../config.json';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  config = config;
 }

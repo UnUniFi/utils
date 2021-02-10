038153db9bdfacf338128ecded560bbf7c3ff4e7
Kimura Yu <33382781+KimuraYu45z@users.noreply.github.com>
2021-02-10 18:29:17 +0900

    update

--- projects/cosmoscan-extension/src/view/keys/key-select-dialog/key-select-dialog.component.ts
+++ projects/cosmoscan-extension/src/view/keys/key-select-dialog/key-select-dialog.component.ts
@@ -1,6 +1,6 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
-import { Key } from '@model/keys/key.model';
+import { Key } from '@model-ce/keys/key.model';
 import * as crypto from 'crypto';
 
 @Component({

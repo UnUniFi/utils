865ad37d355f52ed2316662e2963b08f8dcf961e
YasunoriMATSUOKA <yasunori.matsuoka@gmail.com>
2021-07-05 12:12:36 +0900

    fix: config.js websocketURL

--- projects/cosmoscan-extension/editions/eurx/config.js
+++ projects/cosmoscan-extension/editions/eurx/config.js
@@ -1,6 +1,6 @@
 const config = {
   restURL: `${location.protocol}//${location.hostname}:1317`,
-  websocketURL: `${location.protocol.replace('http', 'ws')}://${location.hostname}:26657`,
+  websocketURL: `${location.protocol.replace('http', 'ws')}//${location.hostname}:26657`,
   chainID: 'eurx-1-test',
   bech32Prefix: {
     accAddr: 'eurx',

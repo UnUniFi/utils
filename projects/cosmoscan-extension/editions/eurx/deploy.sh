83a495bd40578033160bcd5a1a90561c0fc75315
YasunoriMATSUOKA <yasunori.matsuoka@gmail.com>
2021-07-08 11:02:20 +0900

    feat: cosmoscan deploy shellscript

---
+++ projects/cosmoscan-extension/editions/eurx/deploy.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+cd /root/eurx-cosmoscan
+docker-compose down
+docker pull lcnem/cosmoscan
+docker pull lcnem/cosmoscan-eurx
+curl -O https://raw.githubuserconteant.com/lcnem/botany/main/projects/cosmoscan-extension/editions/eurx/docker-compose.yml
+curl -O https://raw.githubusercontent.com/lcnem/botany/main/projects/cosmoscan-extension/nginx.conf
+curl -O https://raw.githubusercontent.com/lcnem/botany/main/projects/cosmoscan-extension/editions/eurx/config.js
+docker-compose up -d

00dd8833062bdef4fdec88520e188dd2e10f2b50
YasunoriMATSUOKA <yasunori.matsuoka@gmail.com>
2021-07-08 11:44:16 +0900

    fix: add tag

--- projects/cosmoscan-extension/editions/jpyx/deploy.sh
+++ projects/cosmoscan-extension/editions/jpyx/deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 cd ~/jpyx-cosmoscan
 docker-compose down
-docker pull lcnem/cosmoscan
-docker pull lcnem/cosmoscan-jpyx
+docker pull lcnem/cosmoscan:latest
+docker pull lcnem/cosmoscan-jpyx:latest
 curl -O https://raw.githubusercontent.com/lcnem/botany/main/projects/cosmoscan-extension/editions/jpyx/docker-compose.yml
 curl -O https://raw.githubusercontent.com/lcnem/botany/main/projects/cosmoscan-extension/nginx.conf
 curl -O https://raw.githubusercontent.com/lcnem/botany/main/projects/cosmoscan-extension/editions/jpyx/config.js

b2c203bf4cbc68abfd9e1dbbecbf940f4cb61519
YasunoriMATSUOKA <yasunori.matsuoka@gmail.com>
2021-07-08 11:38:58 +0900

    fix: typo

--- projects/cosmoscan-extension/editions/eurx/deploy.sh
+++ projects/cosmoscan-extension/editions/eurx/deploy.sh
@@ -3,7 +3,7 @@ cd /root/eurx-cosmoscan
 docker-compose down
 docker pull lcnem/cosmoscan
 docker pull lcnem/cosmoscan-eurx
-curl -O https://raw.githubuserconteant.com/lcnem/botany/main/projects/cosmoscan-extension/editions/eurx/docker-compose.yml
+curl -O https://raw.githubusercontent.com/lcnem/botany/main/projects/cosmoscan-extension/editions/eurx/docker-compose.yml
 curl -O https://raw.githubusercontent.com/lcnem/botany/main/projects/cosmoscan-extension/nginx.conf
 curl -O https://raw.githubusercontent.com/lcnem/botany/main/projects/cosmoscan-extension/editions/eurx/config.js
 docker-compose up -d
--- projects/cosmoscan-extension/editions/jpyx/deploy.sh
+++ projects/cosmoscan-extension/editions/jpyx/deploy.sh
@@ -3,7 +3,7 @@ cd /root/jpyx-cosmoscan
 docker-compose down
 docker pull lcnem/cosmoscan
 docker pull lcnem/cosmoscan-jpyx
-curl -O https://raw.githubuserconteant.com/lcnem/botany/main/projects/cosmoscan-extension/editions/jpyx/docker-compose.yml
+curl -O https://raw.githubusercontent.com/lcnem/botany/main/projects/cosmoscan-extension/editions/jpyx/docker-compose.yml
 curl -O https://raw.githubusercontent.com/lcnem/botany/main/projects/cosmoscan-extension/nginx.conf
 curl -O https://raw.githubusercontent.com/lcnem/botany/main/projects/cosmoscan-extension/editions/jpyx/config.js
 docker-compose up -d

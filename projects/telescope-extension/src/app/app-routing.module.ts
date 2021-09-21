e9792af2b48f311d1b628160b2d34b413860ca31
taro04 <75844498+taro04@users.noreply.github.com>
2021-09-21 22:17:13 +0900

    feat: validation

--- projects/telescope-extension/src/app/app-routing.module.ts
+++ projects/telescope-extension/src/app/app-routing.module.ts
@@ -21,4 +21,4 @@ const routes: Routes = [
   imports: [RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' })],
   exports: [RouterModule],
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
--- projects/telescope-extension/src/view/faucet/faucet.module.ts
+++ projects/telescope-extension/src/view/faucet/faucet.module.ts
@@ -2,13 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FaucetComponent } from './faucet.component';
 import { RouterModule } from '@angular/router';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { MaterialModule } from '@view-ce/material.module';
 
 @NgModule({
   declarations: [FaucetComponent],
-  imports: [CommonModule, RouterModule, FormsModule, FlexLayoutModule, MaterialModule],
+  imports: [CommonModule, RouterModule, FormsModule, ReactiveFormsModule, FlexLayoutModule, MaterialModule],
   exports: [FaucetComponent],
 })
-export class FaucetModule {}
+export class FaucetModule { }

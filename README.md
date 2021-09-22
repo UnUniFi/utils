baf284fc4e76e9e8f5341fd7c1610da876f1d7a4
Kimura Yu <33382781+KimuraYu45z@users.noreply.github.com>
2021-09-22 22:56:24 +0900

    fix: actions

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 
 Utility tools for project Botany (JPYX and EURX).
 
+- main: Website of botany
 - [botany-client](./projects/botany-client/README.md)
 - [telescope-extension](./projects/telescope-extension/README.md)
 - [pricefeed](./projects/pricefeed/README.md)
--- projects/telescope-extension/jest.config.js
+++ projects/telescope-extension/jest.config.js
@@ -3,5 +3,8 @@ require('jest-preset-angular/ngcc-jest-processor');
 module.exports = {
   preset: 'jest-preset-angular',
   setupFilesAfterEnv: ['<rootDir>/projects/telescope-extension/src/setup-jest.ts'],
-  testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/projects/telescope-extension/e2e/']
+  testPathIgnorePatterns: [
+    '<rootDir>/node_modules/',
+    '<rootDir>/projects/telescope-extension/cypress/',
+  ],
 };

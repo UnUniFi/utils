ccffde380ed415662a3db7f5e265b18d1675a1b5
Kimura Yu <33382781+KimuraYu45z@users.noreply.github.com>
2021-09-22 11:37:32 +0900

    fix: build telescope

--- projects/telescope-extension/src/app/cdp/cdps/cdp/cdp.component.ts
+++ projects/telescope-extension/src/app/cdp/cdps/cdp/cdp.component.ts
@@ -7,10 +7,13 @@ import {
 } from '../../../../utils/stream';
 import { getWithdrawLimit, getIssueLimit } from '../../../../utils/function';
 import { cosmosclient } from 'cosmos-client';
-import { rest, botany } from 'botany-client'
+import { rest, botany } from 'botany-client';
 import { combineLatest, from, Observable, pipe, zip } from 'rxjs';
 import { map, mergeMap } from 'rxjs/operators';
-import { InlineResponse2004Cdp1, InlineResponse2006Deposits } from 'projects/botany-client/src/openapi-eurx';
+import {
+  InlineResponse2004Cdp1,
+  InlineResponse2006Deposits,
+} from 'botany-client/esm/openapi-eurx';
 
 @Component({
   selector: 'app-cdp',
@@ -31,15 +34,19 @@ export class CdpComponent implements OnInit {
 
   constructor(
     private readonly route: ActivatedRoute,
-    private readonly cosmosSdk: CosmosSDKService,
+    private readonly cosmosSdk: CosmosSDKService
   ) {
     this.owner$ = this.route.params.pipe(map((params) => params['owner']));
     this.denom$ = this.route.params.pipe(map((params) => params['denom']));
-    const ownerAndDenom$ = combineLatest([this.owner$, this.denom$, this.cosmosSdk.sdk$]);
+    const ownerAndDenom$ = combineLatest([
+      this.owner$,
+      this.denom$,
+      this.cosmosSdk.sdk$,
+    ]);
 
     this.params$ = this.cosmosSdk.sdk$.pipe(
-      mergeMap(sdk => rest.botany.cdp.params(sdk.rest)),
-      map((data) => data.data.params!),
+      mergeMap((sdk) => rest.botany.cdp.params(sdk.rest)),
+      map((data) => data.data.params!)
     );
 
     this.cdp$ = ownerAndDenom$.pipe(
@@ -47,10 +54,10 @@ export class CdpComponent implements OnInit {
         rest.botany.cdp.cdp(
           sdk.rest,
           cosmosclient.AccAddress.fromString(ownerAddr),
-          denom,
-        ),
+          denom
+        )
       ),
-      map(res => res.data.cdp!),
+      map((res) => res.data.cdp!)
     );
 
     this.deposits$ = ownerAndDenom$.pipe(
@@ -58,36 +65,34 @@ export class CdpComponent implements OnInit {
         rest.botany.cdp.allDeposits(
           sdk.rest,
           cosmosclient.AccAddress.fromString(ownerAddr),
-          denom,
-        ),
+          denom
+        )
       ),
-      map(res => res.data.deposits || []),
+      map((res) => res.data.deposits || [])
     );
 
     this.spotPrice$ = this.cosmosSdk.sdk$.pipe(
-      mergeMap(sdk => getSpotPriceStream(
-        sdk.rest,
-        this.denom$,
-        this.params$,
-      )));
+      mergeMap((sdk) => getSpotPriceStream(sdk.rest, this.denom$, this.params$))
+    );
 
     this.liquidationPrice$ = this.cosmosSdk.sdk$.pipe(
-      mergeMap(sdk => getLiquidationPriceStream(
-        sdk.rest,
-        this.denom$,
-        this.params$,
-      )));
+      mergeMap((sdk) =>
+        getLiquidationPriceStream(sdk.rest, this.denom$, this.params$)
+      )
+    );
 
     this.withdrawLimit$ = zip(this.cdp$, this.params$, this.spotPrice$).pipe(
-      map(([cdp, params, price]) => getWithdrawLimit(cdp.cdp!, params, price)),
+      map(([cdp, params, price]) => getWithdrawLimit(cdp.cdp!, params, price))
     );
 
     this.issueLimit$ = zip(
       this.cdp$,
       this.params$,
-      this.liquidationPrice$,
-    ).pipe(map(([cdp, params, price]) => getIssueLimit(cdp.cdp!, params, price)));
+      this.liquidationPrice$
+    ).pipe(
+      map(([cdp, params, price]) => getIssueLimit(cdp.cdp!, params, price))
+    );
   }
 
-  ngOnInit(): void { }
+  ngOnInit(): void {}
 }
--- projects/telescope-extension/src/utils/function.ts
+++ projects/telescope-extension/src/utils/function.ts
@@ -1,80 +1,76 @@
-import { botany } from 'botany-client'
-import { InlineResponse2004Cdp, } from 'projects/botany-client/src/openapi-eurx';
+import { botany } from 'botany-client';
+import { InlineResponse2004Cdp } from 'botany-client/esm/openapi-eurx';
 
 export const getWithdrawLimit = (
   cdp: InlineResponse2004Cdp,
   cdpParams: botany.cdp.IParams,
-  spotPrice: botany.pricefeed.ICurrentPrice,
+  spotPrice: botany.pricefeed.ICurrentPrice
 ) => {
   const collateralDenom = cdp.collateral?.denom;
   const currentCollateralAmount = Number.parseInt(cdp.collateral?.amount!);
   const currentPrincipalAmount = Number.parseInt(cdp.principal?.amount!);
-  const currentAccumulatedFees = Number.parseInt(
-    cdp.accumulated_fees?.amount!,
-  );
+  const currentAccumulatedFees = Number.parseInt(cdp.accumulated_fees?.amount!);
   const principalTotal = currentPrincipalAmount + currentAccumulatedFees;
   const principalConversionFactor = Number.parseInt(
-    cdpParams.debt_param?.conversion_factor || '0',
+    cdpParams.debt_param?.conversion_factor || '0'
   );
 
   const collateralParams = cdpParams.collateral_params?.find(
-    (param) => param.denom === collateralDenom,
+    (param) => param.denom === collateralDenom
   );
   if (collateralParams === undefined) {
     throw new Error(`Parameters for ${collateralDenom} not found`);
   }
   const liquidationRatio = Number.parseFloat(
-    collateralParams?.liquidation_ratio || '0',
+    collateralParams?.liquidation_ratio || '0'
   );
   const collateralConversionFactor = Number.parseInt(
-    collateralParams?.conversion_factor || '0',
+    collateralParams?.conversion_factor || '0'
   );
   const price = Number.parseFloat(spotPrice?.price || '0');
 
   const conversionFactor = Math.pow(
     10,
-    collateralConversionFactor - principalConversionFactor,
+    collateralConversionFactor - principalConversionFactor
   );
 
   return Math.floor(
     currentCollateralAmount -
-    (liquidationRatio * principalTotal * conversionFactor) / price,
+      (liquidationRatio * principalTotal * conversionFactor) / price
   );
 };
 
 export const getIssueLimit = (
   cdp: InlineResponse2004Cdp,
   cdpParams: botany.cdp.IParams,
-  liquidationPrice: botany.pricefeed.ICurrentPrice,
+  liquidationPrice: botany.pricefeed.ICurrentPrice
 ) => {
   const collateralDenom = cdp.collateral?.denom;
   const currentCollateralAmount = Number.parseInt(cdp.collateral?.amount!);
   const currentPrincipalAmount = Number.parseInt(cdp.principal?.amount!);
-  const currentAccumulatedFees = Number.parseInt(
-    cdp.accumulated_fees?.amount!,
-  );
+  const currentAccumulatedFees = Number.parseInt(cdp.accumulated_fees?.amount!);
   const principalTotal = currentPrincipalAmount + currentAccumulatedFees;
   const principalConversionFactor = Number.parseInt(
-    cdpParams.debt_param?.conversion_factor || '0',
+    cdpParams.debt_param?.conversion_factor || '0'
   );
 
   const collateralParams = cdpParams.collateral_params?.find(
-    (param) => param.denom === collateralDenom,
+    (param) => param.denom === collateralDenom
   );
   if (collateralParams === undefined) {
     throw new Error(`Parameters for ${collateralDenom} not found`);
   }
   const liquidationRatio = Number.parseFloat(
-    collateralParams.liquidation_ratio || '0',
+    collateralParams.liquidation_ratio || '0'
   );
   const collateralConversionFactor = Number.parseInt(
-    collateralParams.conversion_factor || '0',
+    collateralParams.conversion_factor || '0'
   );
   const price = Number.parseFloat(liquidationPrice.price!);
 
   const conversionFactor = Math.pow(
     10,
-    principalConversionFactor - collateralConversionFactor,
+    principalConversionFactor - collateralConversionFactor
   );
 
   console.log({
@@ -87,6 +83,6 @@ export const getIssueLimit = (
 
   return Math.floor(
     (currentCollateralAmount * price * conversionFactor) / liquidationRatio -
-    principalTotal,
+      principalTotal
   );
 };

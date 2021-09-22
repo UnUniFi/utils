182daf60bc63916e52b484d7431e5477da421c2d
Kimura Yu <33382781+KimuraYu45z@users.noreply.github.com>
2021-09-22 22:47:52 +0900

    feat: confirm build and work

--- projects/main/src/app/pages/apply-validator/apply-validator.module.ts
+++ projects/main/src/app/pages/apply-validator/apply-validator.module.ts
@@ -1,9 +1,8 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-
+import { ApplyValidatorModule } from '../../views/apply-validator/apply-validator.module';
 import { ApplyValidatorRoutingModule } from './apply-validator-routing.module';
 import { ApplyValidatorComponent } from './apply-validator.component';
-import { ApplyValidatorModule } from '@view-w/apply-validator/apply-validator.module';
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 
 @NgModule({
   declarations: [ApplyValidatorComponent],

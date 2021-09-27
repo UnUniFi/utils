24ca2cefa7782ea1d4a4d08f9cbd01b725f49862
Kimura Yu <33382781+KimuraYu45z@users.noreply.github.com>
2021-09-27 11:38:25 +0900

    feat: app.component

--- projects/telescope-extension/src/app/app.module.ts
+++ projects/telescope-extension/src/app/app.module.ts
@@ -1,18 +1,32 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { KeySelectDialogModule } from './views/keys/key-select-dialog/key-select-dialog.module';
+import { ViewModule } from './views/view.module';
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoadingDialogModule } from 'ng-loading-dialog';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatDialogModule,
+    MatSnackBarModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    FormsModule,
+    LoadingDialogModule,
+    KeySelectDialogModule,
+    ViewModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}

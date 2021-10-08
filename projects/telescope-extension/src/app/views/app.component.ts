c3faef89d2871bd25b8711ec7bcc2534b3c66a2d
YasunoriMATSUOKA <yasunori.matsuoka@gmail.com>
2021-10-08 10:39:44 +0900

    fix: lint

--- projects/telescope-extension/src/app/views/app.component.ts
+++ projects/telescope-extension/src/app/views/app.component.ts
@@ -1,16 +1,9 @@
-import {
-  Component,
-  OnInit,
-  ViewChild,
-  Input,
-  Output,
-  EventEmitter,
-} from '@angular/core';
-import { Observable, combineLatest } from 'rxjs';
+import { Component, OnInit, ViewChild, Input, Output, EventEmitter } from '@angular/core';
 import { MediaObserver } from '@angular/flex-layout';
-import { map } from 'rxjs/operators';
-import { Router, NavigationEnd } from '@angular/router';
 import { MatDrawerMode, MatSidenav } from '@angular/material/sidenav';
+import { Router, NavigationEnd } from '@angular/router';
+import { Observable, combineLatest } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'view-app',
@@ -40,29 +33,21 @@ export class AppComponent implements OnInit {
     this.drawerMode$ = this.mediaObserver
       .asObservable()
       .pipe(
-        map((changes) =>
-          changes.find((change) => change.mqAlias === 'xs') ? 'over' : 'side',
-        ),
+        map((changes) => (changes.find((change) => change.mqAlias === 'xs') ? 'over' : 'side')),
       );
 
     this.drawerOpened$ = this.mediaObserver
       .asObservable()
-      .pipe(
-        map((changes) =>
-          changes.find((change) => change.mqAlias === 'xs') ? false : true,
-        ),
-      );
+      .pipe(map((changes) => (changes.find((change) => change.mqAlias === 'xs') ? false : true)));
 
-    combineLatest([this.drawerMode$, this.router.events]).subscribe(
-      ([drawerMode, event]) => {
-        if (drawerMode === 'over' && event instanceof NavigationEnd) {
-          this.sidenav?.close();
-        }
-      },
-    );
+    combineLatest([this.drawerMode$, this.router.events]).subscribe(([drawerMode, event]) => {
+      if (drawerMode === 'over' && event instanceof NavigationEnd) {
+        this.sidenav?.close();
+      }
+    });
   }
 
-  ngOnInit() { }
+  ngOnInit() {}
 
   onSubmitSearchValue(value: string) {
     this.appSubmitSearchValue.emit(value);

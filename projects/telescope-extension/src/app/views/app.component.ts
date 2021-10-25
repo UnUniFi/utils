c10e42aca7376fe6577f955c6fb57aeb012a5690
taro04 <75844498+taro04@users.noreply.github.com>
2021-10-25 21:01:49 +0900

    feat: add icon to sidenav menu

--- projects/telescope-extension/src/app/views/app.component.ts
+++ projects/telescope-extension/src/app/views/app.component.ts
@@ -10,7 +10,7 @@ import { combineLatest, BehaviorSubject } from 'rxjs';
 })
 export class AppComponent implements OnInit {
   @Input()
-  extensionNavigations?: { name: string; link: string }[];
+  extensionNavigations?: { name: string; link: string; icon: string }[];
 
   @Input()
   searchValue: string;

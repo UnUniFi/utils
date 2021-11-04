bd59ca4322d668addc0f9b589575e8141b2a8f85
Senna Ohmura <29295263+Senna46@users.noreply.github.com>
2021-11-04 21:50:16 +0900

    feat: auction list add bid info

--- projects/telescope-extension/src/app/views/auction/auctions/auctions.module.ts
+++ projects/telescope-extension/src/app/views/auction/auctions/auctions.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { FormsModule } from '@angular/forms';
+import { MatChipsModule } from '@angular/material/chips';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { RouterModule } from '@angular/router';
 import { MaterialModule } from 'projects/telescope-extension/src/app/views/material.module';
@@ -16,6 +17,7 @@ import { MaterialModule } from 'projects/telescope-extension/src/app/views/mater
     FlexLayoutModule,
     MaterialModule,
     MatPaginatorModule,
+    MatChipsModule,
   ],
   exports: [AuctionsComponent],
 })

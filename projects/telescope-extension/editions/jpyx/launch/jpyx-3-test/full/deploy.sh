7e929c39d84d2cd3615006fda5b766a87320b215
YasunoriMATSUOKA <yasunori.matsuoka@gmail.com>
2021-11-08 17:05:11 +0900

    fix: deploy script and readme

--- projects/telescope-extension/editions/jpyx/launch/jpyx-3-test/full/deploy.sh
+++ projects/telescope-extension/editions/jpyx/launch/jpyx-3-test/full/deploy.sh
@@ -5,5 +5,5 @@ docker pull lcnem/telescope:latest
 docker pull lcnem/telescope-jpyx:latest
 curl -O https://raw.githubusercontent.com/lcnem/botany/main/projects/telescope-extension/editions/jpyx/docker-compose.yml
 curl -O https://raw.githubusercontent.com/lcnem/botany/main/projects/telescope-extension/nginx.conf
-curl -O https://raw.githubusercontent.com/lcnem/botany/main/projects/telescope-extension/editions/jpyx/config.js
+curl -O https://raw.githubusercontent.com/lcnem/botany/main/projects/telescope-extension/editions/jpyx/launch/jpyx-3-test/full/config.js
 docker-compose up -d

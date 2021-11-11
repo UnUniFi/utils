b1073d4011f4c399726101b8cda65807dc76c94c
Senna46 <29295263+Senna46@users.noreply.github.com>
2021-11-11 16:28:57 +0900

    feat: key select guard

--- projects/telescope-extension/src/app/pages/auction/auction-routing.module.ts
+++ projects/telescope-extension/src/app/pages/auction/auction-routing.module.ts
@@ -11,6 +11,7 @@ const routes: Routes = [
   {
     path: 'auctions',
     loadChildren: () => import('./auctions/auctions.module').then((mod) => mod.AppAuctionsModule),
+    canActivate: [KeySelectGuard],
   },
 ];
 

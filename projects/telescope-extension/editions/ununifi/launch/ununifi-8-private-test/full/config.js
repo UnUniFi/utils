67ad65e36ee43fd119d9878bd64dffcfd1d4b08a
Senna46 <29295263+Senna46@users.noreply.github.com>
2022-04-05 14:15:23 +0900

    feat: 3type config

--- projects/telescope-extension/editions/ununifi/launch/ununifi-8-private-test/full/config.js
+++ projects/telescope-extension/editions/ununifi/launch/ununifi-8-private-test/full/config.js
@@ -1,3 +1,8 @@
+// Describe node as array
+const nodes = ['a.private-test.ununifi.cauchye.net', 'b.private-test.ununifi.cauchye.net'];
+// Get a random value from an array (ToDo. Select from nodes at the front)
+const node = nodes[Math.floor(Math.random() * nodes.length)];
+
 const config = {
   restURL: `${location.protocol}//${location.hostname}:1317`,
   websocketURL: `${location.protocol.replace('http', 'ws')}//${location.hostname}:26657`,
@@ -20,22 +25,36 @@ const config = {
     faucet: [
       {
         hasFaucet: true,
-        faucetURL: `${location.protocol}//${location.hostname}:8000`,
+        faucetURL: `${location.protocol}//${node}:8000`,
         denom: 'ubtc',
         creditAmount: 100, // amount to credit in max request
         maxCredit: 99, // account has already maxCredit balance cannot claim anymore
       },
       {
         hasFaucet: true,
-        faucetURL: `${location.protocol}//${location.hostname}:8002`,
+        faucetURL: `${location.protocol}//${node}:8002`,
         denom: 'uguu',
         creditAmount: 2000000,
         maxCredit: 1999999,
       },
       {
         hasFaucet: false,
-        faucetURL: `${location.protocol}//${location.hostname}:8004`,
+        faucetURL: `${location.protocol}//${node}:8004`,
         denom: 'jpu',
+        creditAmount: 10,
+        maxCredit: 9,
+      },
+      {
+        hasFaucet: true,
+        faucetURL: `${location.protocol}//${node}:8006`,
+        denom: 'ueth',
+        creditAmount: 1000,
+        maxCredit: 999,
+      },
+      {
+        hasFaucet: false,
+        faucetURL: `${location.protocol}//${node}:8008`,
+        denom: 'euu',
         creditAmount: 10,
         maxCredit: 9,
       },
--- projects/telescope-extension/editions/ununifi/launch/ununifi-8-private-test/light/config.js
+++ projects/telescope-extension/editions/ununifi/launch/ununifi-8-private-test/light/config.js
@@ -1,6 +1,11 @@
+// Describe node as array
+const nodes = ['a.private-test.ununifi.cauchye.net', 'b.private-test.ununifi.cauchye.net'];
+// Get a random value from an array (ToDo. Select from nodes at the front)
+const node = nodes[Math.floor(Math.random() * nodes.length)];
+
 const config = {
-  restURL: `${location.protocol}//${location.hostname}:1317`,
-  websocketURL: `${location.protocol.replace('http', 'ws')}//${location.hostname}:26657`,
+  restURL: `${location.protocol}//${node}:1317`,
+  websocketURL: `${location.protocol.replace('http', 'ws')}//${node}:26657`,
   chainID: 'ununifi-8-private-test',
   bech32Prefix: {
     accAddr: 'ununifi',
@@ -14,31 +19,45 @@ const config = {
     {
       denom: 'uguu',
       amount: 0.015,
-    }
+    },
   ],
   extension: {
     faucet: [
       {
         hasFaucet: true,
-        faucetURL: `${location.protocol}//b.private-test.ununifi.cauchye.net:8000`, // a.test.ununifi.cauchye.net is also available.
+        faucetURL: `${location.protocol}//${node}:8000`,
         denom: 'ubtc',
         creditAmount: 100, // amount to credit in max request
         maxCredit: 99, // account has already maxCredit balance cannot claim anymore
       },
       {
         hasFaucet: true,
-        faucetURL: `${location.protocol}//b.private-test.ununifi.cauchye.net:8002`, // a.test.ununifi.cauchye.net is also available.
+        faucetURL: `${location.protocol}//${node}:8002`,
         denom: 'uguu',
         creditAmount: 2000000,
         maxCredit: 1999999,
       },
       {
         hasFaucet: false,
-        faucetURL: `${location.protocol}//b.private-test.ununifi.cauchye.net:8004`, // a.test.ununifi.cauchye.net is also available.
+        faucetURL: `${location.protocol}//${node}:8004`,
         denom: 'jpu',
         creditAmount: 10,
         maxCredit: 9,
-      }
+      },
+      {
+        hasFaucet: true,
+        faucetURL: `${location.protocol}//${node}:8006`,
+        denom: 'ueth',
+        creditAmount: 1000,
+        maxCredit: 999,
+      },
+      {
+        hasFaucet: false,
+        faucetURL: `${location.protocol}//${node}:8008`,
+        denom: 'euu',
+        creditAmount: 10,
+        maxCredit: 9,
+      },
     ],
     monitor: {
       monitorURL: `${location.protocol}//${location.hostname}:9000`,

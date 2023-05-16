67c8a1c9c273946323ed4221bd9c79c7ba6ffb66
mkXultra <electrincgoat@gmail.com>
2023-05-16 12:35:24 +0900

    fix: gen cmd for v47

--- scripts/utils/chain_init_gen_command.sh
+++ scripts/utils/chain_init_gen_command.sh
@@ -3,6 +3,8 @@
 # if not passing arguments to this script, execute init command
 if [ $# -lt 1 ]; then
   execute_command="init"
+else
+  execute_command=$1
 fi
 
 echo "Executing $execute_command command..."
@@ -37,16 +39,23 @@ if [ "$execute_command" = "init" ]; then
   fi
   exit 0;
 elif [ "$execute_command" = "reset-node" ]; then
+  VAL=my_validator
+  FAUCET=faucet
+  USER1=user1
+  USER2=user2
+  USER3=user3
+  USER4=user4
+  PRICEFEED=pricefeed
   if (( major_version > 0 || minor_version >= 47 )); then
     echo "This is version 0.47 or later."
-    $DAEMON_NAME genesis add-genesis-account my_validator 1000000000000uguu,100000000000ubtc;
-    $DAEMON_NAME genesis add-genesis-account user1 100000000000uguu,100000000000ubtc,50000000000uusdc;
-    $DAEMON_NAME genesis add-genesis-account user2 100000000000uguu,100000000000ubtc,50000000000uusdc;
-    $DAEMON_NAME genesis add-genesis-account user3 100000000000uguu,100000000000ubtc,50000000000uusdc;
-    $DAEMON_NAME genesis add-genesis-account user4 100000000000uguu,100000000000ubtc,50000000000uusdc;
-    $DAEMON_NAME genesis add-genesis-account faucet 50000000000000uguu,500000000000ubtc,50000000000ueth,50000000000000uusdc;
-    $DAEMON_NAME genesis add-genesis-account $PRICEFEED 100000000000uguu,100000000000000ubtc,100000000000000uusdc;
-    $DAEMON_NAME genesis gentx my_validator 1000000000uguu --chain-id $CHAIN_ID --keyring-backend test;
+    $DAEMON_NAME genesis add-genesis-account $($DAEMON_NAME keys show $VAL --keyring-backend test -a) 1000000000000uguu,100000000000ubtc;
+    $DAEMON_NAME genesis add-genesis-account $($DAEMON_NAME keys show $USER1 --keyring-backend test -a) 100000000000uguu,100000000000ubtc,50000000000uusdc;
+    $DAEMON_NAME genesis add-genesis-account $($DAEMON_NAME keys show $USER2 --keyring-backend test -a) 100000000000uguu,100000000000ubtc,50000000000uusdc;
+    $DAEMON_NAME genesis add-genesis-account $($DAEMON_NAME keys show $USER3 --keyring-backend test -a) 100000000000uguu,100000000000ubtc,50000000000uusdc;
+    $DAEMON_NAME genesis add-genesis-account $($DAEMON_NAME keys show $USER4 --keyring-backend test -a) 100000000000uguu,100000000000ubtc,50000000000uusdc;
+    $DAEMON_NAME genesis add-genesis-account $($DAEMON_NAME keys show $FAUCET --keyring-backend test -a) 50000000000000uguu,500000000000ubtc,50000000000ueth,50000000000000uusdc;
+    $DAEMON_NAME genesis add-genesis-account $($DAEMON_NAME keys show $PRICEFEED --keyring-backend test -a) 100000000000uguu,100000000000000ubtc,100000000000000uusdc;
+    $DAEMON_NAME genesis gentx $VAL 1000000000uguu --chain-id $CHAIN_ID --keyring-backend test;
     $DAEMON_NAME genesis collect-gentxs;
   else
     echo "This is before version 0.47."

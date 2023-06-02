6ed89c33ed2518fb05ff112afa8412ce3706ec02
Senna46 <29295263+Senna46@users.noreply.github.com>
2023-06-02 23:36:38 +0900

    feat: add block-storage

---
+++ scripts/vultr/add-block-storage-move-db.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+lsblk
+### Mount blockstorage
+parted -s /dev/vdb mklabel gpt
+parted -s /dev/vdb unit mib mkpart primary 0% 100%
+mkfs.ext4 /dev/vdb1
+mkdir /mnt/blockstorage
+echo >> /etc/fstab
+echo /dev/vdb1 /mnt/blockstorage ext4 defaults,noatime,nofail 0 0 >> /etc/fstab
+mount /mnt/blockstorage
+
+# DAEMON_HOME=~/.ununifi
+
+### Stop cosmvisor
+systemctl stop cosmovisor.service
+cp -rvp $DAEMON_HOME /mnt/blockstorage
+
+
+### Require!! edit lib/systemd/system/cosmovisor.service
+## Environment="DAEMON_HOME=/mnt/blockstorage/.ununifi"
+## ExecStart=/root/go/bin/cosmovisor start --home /mnt/blockstorage/.ununifi
+
+### change current version & directory
+ln -nfs /mnt/blockstorage/.ununifi/cosmovisor/upgrades/v2_1 /mnt/blockstorage/.ununifi/cosmovisor/current
+
+# Restart
+systemctl daemon-reload
+systemctl restart cosmovisor.service
+systemctl status cosmovisor.service
+
+## If you need to monitor the service after launch, you can view the logs using:
+#journalctl -u cosmovisor -f -o cat

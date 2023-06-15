331812edce49b2972c4a83ce1e166e31ab93fe25
Senna46 <29295263+Senna46@users.noreply.github.com>
2023-06-15 18:36:28 +0900

    feat: function

--- projects/pricefeed/src/index.ts
+++ projects/pricefeed/src/index.ts
@@ -1,34 +1,45 @@
-import * as cron from "node-cron";
-import { PriceOracle } from "./price-oracle";
-require("dotenv").config();
+import * as cron from 'node-cron';
+import { PriceOracle } from './price-oracle';
+require('dotenv').config();
 
-import OpenExchangeRatesClient from "./clients/fx/open-exchange-rates";
+import OpenExchangeRatesClient from './clients/fx/open-exchange-rates';
+import { PriceStargaze } from './price-stargaze';
+import { PostPrice } from './post-price';
 
 // Load environment variables
-const marketIDs = (process.env.MARKET_IDS || "").split(",");
-const expiry = process.env.EXPIRY || "";
-const expiryThreshold = process.env.EXPIRY_THRESHOLD || "";
-const deviation = process.env.DEVIATION || "";
-const lcdURL = process.env.LCD_URL || "";
-const chainID = process.env.CHAIN_ID || "";
-const mnemonic = process.env.MNEMONIC || "";
-const bech32Prefix = process.env.BECH32PREFIX || "";
-const cronTab = process.env.CRONTAB || "";
-const openExchangeRateAppId = process.env.OPEN_EXCHANGE_RATES_APP_ID || "";
+const expiry = process.env.EXPIRY || '14400';
+const expiryThreshold = process.env.EXPIRY_THRESHOLD || '300';
+const deviation = process.env.DEVIATION || '';
+const lcdURL = process.env.LCD_URL || '';
+const chainID = process.env.CHAIN_ID || '';
+const mnemonic = process.env.MNEMONIC || '';
+const bech32Prefix = process.env.BECH32PREFIX || '';
+const cronTab = process.env.CRONTAB || '';
+const openExchangeRateAppId = process.env.OPEN_EXCHANGE_RATES_APP_ID || '';
 
-const dataProviderType = process.env.DATA_PROVIDER_TYPE || "default";
-const dataProviderUrl = process.env.DATA_PROVIDER_URL || "https://laozi1.bandchain.org";
-const dataProviderDataRetentionPeriodMin = process.env.DATA_DATA_RETENTION_PERIOD_MIN || "30";
-const dataProviderStoreType = process.env.DATA_PROVIDER_STORE_TYPE || "memory";
-const dataProviderStoreLocation = process.env.DATA_PROVIDER_STORE_LOCATION || "./price_data.json";
-const dataProviderConf  =  {
+// oracle
+const marketIDs = (process.env.MARKET_IDS || '').split(',');
+const dataProviderType = process.env.DATA_PROVIDER_TYPE || 'default';
+const dataProviderUrl = process.env.DATA_PROVIDER_URL || 'https://laozi1.bandchain.org';
+const dataProviderDataRetentionPeriodMin = process.env.DATA_DATA_RETENTION_PERIOD_MIN || '30';
+const dataProviderStoreType = process.env.DATA_PROVIDER_STORE_TYPE || 'memory';
+const dataProviderStoreLocation = process.env.DATA_PROVIDER_STORE_LOCATION || './price_data.json';
+const dataProviderConf = {
   dataProviderType,
   dataProviderUrl,
   dataProviderDataRetentionPeriodMin,
   dataProviderStoreType,
   dataProviderStoreLocation,
-}
-const IS_DEBUG_MODE = process.env.MODE == "debug";
+};
+
+// stargaze
+const collectionIDs = (process.env.COLLECTION_IDS || '').split(',');
+const stargazeRest = process.env.STARGAZE_REST || 'https://rpc.stargaze-apis.com/';
+const marketplaceAddr =
+  process.env.MARKETPLACE_ADDR ||
+  'stars1fvhcnyddukcqfnt7nlwv3thm5we22lyxyxylr9h77cvgkcn43xfsvgv0pl';
+
+const IS_DEBUG_MODE = process.env.MODE == 'debug';
 
 const fxClients = [];
 if (openExchangeRateAppId) {
@@ -46,14 +57,22 @@ const oracle = new PriceOracle(
   mnemonic,
   bech32Prefix,
   fxClients,
-  dataProviderConf
+  dataProviderConf,
 );
 
-cron.schedule(cronTab, () => {
-  oracle.postPrices();
+const stargaze = new PriceStargaze(collectionIDs, stargazeRest, marketplaceAddr);
+
+const postPrice = new PostPrice(expiry, lcdURL, chainID, mnemonic, bech32Prefix);
+
+cron.schedule(cronTab, async () => {
+  // todo : use postPrice.postPrices
+  await oracle.postPrices();
+
+  const floorPrices = await stargaze.getPrices();
+  await postPrice.postPrices(floorPrices);
 });
 
 // if DEBUG_MODE is true, post prices immediately
-if(IS_DEBUG_MODE){
+if (IS_DEBUG_MODE) {
   oracle.postPrices();
 }
---
+++ projects/pricefeed/src/post-price.ts
@@ -0,0 +1,254 @@
+import { CurrentPriceData } from './domain/market-price';
+import { cosmosclient, rest, proto } from '@cosmos-client/core';
+import Long from 'long';
+import { ununifi, google } from 'ununifi-client';
+import { AccAddress } from '@cosmos-client/core/cjs/types';
+
+require('dotenv').config();
+require('log-timestamp');
+const IS_DEBUG_MODE = process.env.MODE == 'debug';
+
+/**
+ * Post Price data to UnUniFi chain.
+ */
+export class PostPrice {
+  private sdk: cosmosclient.CosmosSDK;
+  private privKey: Promise<proto.cosmos.crypto.secp256k1.PrivKey>;
+
+  constructor(
+    private expiry: string,
+    url: string,
+    chainID: string,
+    mnemonic: string,
+    bech32Prefix: string,
+  ) {
+    if (!expiry) {
+      throw new Error('must specify an expiration time');
+    }
+
+    this.sdk = new cosmosclient.CosmosSDK(url, chainID);
+    this.privKey = cosmosclient
+      .generatePrivKeyFromMnemonic(mnemonic)
+      .then((buffer) => new proto.cosmos.crypto.secp256k1.PrivKey({ key: buffer }));
+    if (bech32Prefix) {
+      cosmosclient.config.setBech32Prefix({
+        accAddr: bech32Prefix,
+        accPub: bech32Prefix + cosmosclient.AddressPrefix.Public,
+        valAddr:
+          bech32Prefix + cosmosclient.AddressPrefix.Validator + cosmosclient.AddressPrefix.Operator,
+        valPub:
+          bech32Prefix +
+          cosmosclient.AddressPrefix.Validator +
+          cosmosclient.AddressPrefix.Operator +
+          cosmosclient.AddressPrefix.Public,
+        consAddr:
+          bech32Prefix +
+          cosmosclient.AddressPrefix.Validator +
+          cosmosclient.AddressPrefix.Consensus,
+        consPub:
+          bech32Prefix +
+          cosmosclient.AddressPrefix.Validator +
+          cosmosclient.AddressPrefix.Consensus +
+          cosmosclient.AddressPrefix.Public,
+      });
+    }
+  }
+
+  /**
+   * Post prices for each market
+   */
+  async postPrices(prices: CurrentPriceData[]) {
+    const privKey = await this.privKey;
+    const address = cosmosclient.AccAddress.fromPublicKey(privKey.pubKey());
+    const account = await rest.auth
+      .account(this.sdk, address)
+      .then((res) => res.data.account && cosmosclient.codec.unpackCosmosAny(res.data.account));
+
+    if (!(account instanceof proto.cosmos.auth.v1beta1.BaseAccount)) {
+      throw Error('not a BaseAccount');
+    }
+
+    // await this.getLatestFiatCurrencyPrices();
+
+    for (const priceData of prices) {
+      try {
+        const res: any = await this.postNewPrice(priceData.price, priceData.marketID, address);
+        if (IS_DEBUG_MODE) {
+          console.log('res', res);
+        }
+        // if (res.data.code !== undefined) {
+        //   throw new Error(res.data.raw_log);
+        // }
+      } catch (e) {
+        console.log(`could not post ${priceData.marketID} price`);
+        console.log(e);
+        return;
+      }
+    }
+  }
+
+  /**
+   * Posts a new price for a market ID
+   * @param {String} fetchedPrice the fetched price
+   * @param {String} marketID the market's ID
+   * @param {String} account the oracle's account information
+   * @param {Number} index the iteration count of the market IDs
+   */
+  async postNewPrice(
+    fetchedPrice: number,
+    marketID: string,
+    address: AccAddress,
+    // account: proto.cosmos.auth.v1beta1.BaseAccount,
+    // index: number,
+  ) {
+    if (!fetchedPrice) {
+      throw new Error('a retreived price is required in order to post a new price');
+    }
+
+    // Set up post price transaction parameters
+    const newPrice = fetchedPrice.toFixed(18).toString();
+    let expiryDate = new Date();
+    expiryDate = new Date(expiryDate.getTime() + Number.parseInt(this.expiry) * 1000);
+    const account = await rest.auth
+      .account(this.sdk, address)
+      .then((res) => res.data.account && cosmosclient.codec.unpackCosmosAny(res.data.account));
+
+    if (!(account instanceof proto.cosmos.auth.v1beta1.BaseAccount)) {
+      throw Error('not a BaseAccount');
+    }
+    const sequence = account.sequence;
+
+    console.log(`posting price ${newPrice} for ${marketID} with sequence ${sequence.toString()}`);
+
+    const privKey = await this.privKey;
+
+    // build tx
+    const msgPostPrice = new ununifi.pricefeed.MsgPostPrice({
+      from: account.address,
+      market_id: marketID,
+      price: newPrice,
+      expiry: new proto.google.protobuf.Timestamp({
+        seconds: Long.fromNumber(expiryDate.getTime() / 1000),
+      }),
+    });
+
+    const txBody = new proto.cosmos.tx.v1beta1.TxBody({
+      messages: [cosmosclient.codec.packAny(msgPostPrice)],
+    });
+
+    // auth info for simulation
+    const simulatedAuthInfo = new proto.cosmos.tx.v1beta1.AuthInfo({
+      signer_infos: [
+        {
+          public_key: cosmosclient.codec.packAny(privKey.pubKey()),
+          mode_info: {
+            single: {
+              mode: proto.cosmos.tx.signing.v1beta1.SignMode.SIGN_MODE_DIRECT,
+            },
+          },
+          sequence: sequence,
+        },
+      ],
+      fee: {
+        amount: [
+          {
+            denom: process.env.MINIMUM_GAS_PRICE_DENOM,
+            amount: '1',
+          },
+        ],
+        gas_limit: cosmosclient.Long.fromString('1'),
+      },
+    });
+
+    const simulatedTxBuilder = new cosmosclient.TxBuilder(this.sdk, txBody, simulatedAuthInfo);
+    const simulatedSignDocBytes = simulatedTxBuilder.signDocBytes(account.account_number);
+    simulatedTxBuilder.addSignature(privKey.sign(simulatedSignDocBytes));
+    const txForSimulation = JSON.parse(simulatedTxBuilder.cosmosJSONStringify());
+    delete txForSimulation.auth_info.signer_infos[0].mode_info.multi;
+
+    // Note: google.protobuf.Timestamp type must be converted to rfc3339 string, because it is unmarshaled in backend go process.
+    const googleProtobufTimestamp = google.protobuf.Timestamp.fromObject(
+      txForSimulation.body.messages[0].expiry,
+    );
+    const goTimeString = cosmosclient.codec.protobufTimestampToJsDate(googleProtobufTimestamp);
+    txForSimulation.body.messages[0].expiry = goTimeString;
+
+    let simulatedResult;
+    let gas: proto.cosmos.base.v1beta1.ICoin;
+    let fee: proto.cosmos.base.v1beta1.ICoin;
+
+    // simulate
+    try {
+      simulatedResult = await rest.tx.simulate(this.sdk, {
+        tx: txForSimulation,
+        tx_bytes: simulatedTxBuilder.txBytes(),
+      });
+      // console.log('simulate');
+      // console.log(simulatedResult);
+      const simulatedGasUsed = simulatedResult.data.gas_info?.gas_used;
+      const simulatedGasUsedWithMarginNumber = simulatedGasUsed
+        ? parseInt(simulatedGasUsed) * 1.1
+        : 200000;
+      const simulatedGasUsedWithMargin = Math.ceil(simulatedGasUsedWithMarginNumber).toString();
+      const simulatedFeeWithMarginNumber =
+        parseInt(simulatedGasUsedWithMargin) *
+        parseFloat(
+          process.env.MINIMUM_GAS_PRICE_AMOUNT ? process.env.MINIMUM_GAS_PRICE_AMOUNT : '0',
+        );
+      const simulatedFeeWithMargin = Math.ceil(simulatedFeeWithMarginNumber).toString();
+      // console.log({
+      // simulatedGasUsed,
+      // simulatedGasUsedWithMargin,
+      // simulatedFeeWithMarginNumber,
+      // simulatedFeeWithMargin,
+      // });
+      gas = {
+        denom: process.env.MINIMUM_GAS_PRICE_DENOM,
+        amount: simulatedGasUsedWithMargin,
+      };
+      fee = {
+        denom: process.env.MINIMUM_GAS_PRICE_DENOM,
+        amount: simulatedFeeWithMargin,
+      };
+    } catch (e) {
+      console.error(e);
+      return;
+    }
+
+    // auth info for announce
+    const authInfo = new proto.cosmos.tx.v1beta1.AuthInfo({
+      signer_infos: [
+        {
+          public_key: cosmosclient.codec.packAny(privKey.pubKey()),
+          mode_info: {
+            single: {
+              mode: proto.cosmos.tx.signing.v1beta1.SignMode.SIGN_MODE_DIRECT,
+            },
+          },
+          sequence: sequence,
+        },
+      ],
+      fee: {
+        amount: [fee],
+        gas_limit: cosmosclient.Long.fromString(gas.amount ? gas.amount : '200000'),
+      },
+    });
+
+    // sign
+    const txBuilder = new cosmosclient.TxBuilder(this.sdk, txBody, authInfo);
+    const signDocBytes = txBuilder.signDocBytes(account.account_number);
+    txBuilder.addSignature(privKey.sign(signDocBytes));
+
+    // broadcast
+    try {
+      const res = await rest.tx.broadcastTx(this.sdk, {
+        tx_bytes: txBuilder.txBytes(),
+        mode: rest.tx.BroadcastTxMode.Block,
+      });
+      console.log('broadcast');
+      console.log(res);
+    } catch (e) {
+      console.error(e);
+    }
+  }
+}

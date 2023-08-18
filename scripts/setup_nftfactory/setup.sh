bd6efea8ea150f5e94f0a1ea58034f6613b2969b
Senna46 <29295263+Senna46@users.noreply.github.com>
2023-08-18 22:42:38 +0900

    fix: err

--- scripts/setup_nftfactory/setup.sh
+++ scripts/setup_nftfactory/setup.sh
@@ -2,4 +2,4 @@
 NODE_HOME=$HOME/.ununifi
 
 jq '.app_state.nftfactory.params.class_creation_fee = []' $NODE_HOME/config/genesis.json > temp.json ; mv temp.json $NODE_HOME/config/genesis.json;
-jq '.app_state.nftfactory.params.fee_collector_address = "' $NODE_HOME/config/genesis.json > temp.json ; mv temp.json $NODE_HOME/config/genesis.json;
+jq '.app_state.nftfactory.params.fee_collector_address = ""' $NODE_HOME/config/genesis.json > temp.json ; mv temp.json $NODE_HOME/config/genesis.json;
